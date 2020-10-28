94f617f6d51de48f04459af961c0e84abbd404c8
sgulania <gulania@usc.edu>
2020-10-28 00:10:40 -0700

    update

--- BCH.py
+++ BCH.py
@@ -1,32 +1,70 @@
 from sympy import *                                                             
-from sympy.physics.secondquant import F, Fd,Commutator,AntiSymmetricTensor      
-from sympy.physics.quantum import Commutator, Dagger, Operator                  
-from fractions import Fraction                                                  
-                                                                                
-def level(main,expr):                                                           
- #setup T                                                                        
- k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12 = symbols('k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12' , below_fermi=True, cls=Dummy)
- d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12 = symbols('d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12' , above_fermi=True, cls=Dummy)
-                                                                                
- TA1 = AntiSymmetricTensor('T',(d1,),(k1,))*Fd(d1)*F(k1)                       
- TA2 = Fraction(1, 4)*AntiSymmetricTensor('T',(d2,d3),(k2,k3))*Fd(d2)*Fd(d3)*F(k3)*F(k2)
- TA  = TA1+TA2                                                                  
-                                                                                
- TB1 = AntiSymmetricTensor('T',(d4,),(k4,))*Fd(d4)*F(k4)                       
- TB2 = Fraction(1, 4)*AntiSymmetricTensor('T',(d5,d6),(k5,k6))*Fd(d5)*Fd(d6)*F(k6)*F(k5)
- TB  = TB1+TB2                                                                  
-                                                                                
- TC1 = AntiSymmetricTensor('T',(d7,),(k7,))*Fd(d7)*F(k7)                       
- TC2 = Fraction(1, 4)*AntiSymmetricTensor('T',(d8,d9),(k8,k9))*Fd(d8)*Fd(d9)*F(k9)*F(k8)
- TC  = TC1+TC2                                                                  
-                                                                                
- TD1 = AntiSymmetricTensor('T',(d10,),(k10,))*Fd(d10)*F(k10)                   
- TD2 = Fraction(1, 4)*AntiSymmetricTensor('T',(d11,d12),(k11,k12))*Fd(d11)*Fd(d12)*F(k11)*F(k12)
- TD = TD1+TD2                                                                   
-                                                                                
- # BCH expansion                                                                
- if expr == "SD":                                                               
-  BCH_expansion = main + Commutator(main,TA) + Fraction(1, 2)*Commutator(Commutator(main,TA),TB) + \
-                                Fraction(1, 6)*Commutator(Commutator(Commutator(main,TA),TB),TC) + \
-                                Fraction(1, 24)*Commutator(Commutator(Commutator(Commutator(main,TA),TB),TC),TD)
-  return BCH_expansion
+from sympy.physics.secondquant import F, Fd,wicks,Commutator,AntiSymmetricTensor,NO,evaluate_deltas                  
+from sympy.physics.secondquant import substitute_dummies
+from sympy.physics.quantum import Commutator, Dagger, Operator
+from fractions import Fraction
+from IPython.display import display, Markdown
+
+
+def get_CC_operators():
+    """
+    Returns a tuple (T1,T2) of unique operators.
+    """
+    i = symbols('i', below_fermi=True, cls=Dummy)
+    a = symbols('a', above_fermi=True, cls=Dummy)
+    t_ai = AntiSymmetricTensor('t', (a,), (i,))
+    ai = NO(Fd(a)*F(i))
+    i, j = symbols('i,j', below_fermi=True, cls=Dummy)
+    a, b = symbols('a,b', above_fermi=True, cls=Dummy)
+    t_abij = AntiSymmetricTensor('t', (a, b), (i, j))
+    abji = NO(Fd(a)*Fd(b)*F(j)*F(i))
+
+    T1 = t_ai*ai
+    T2 = Rational(1, 4)*t_abij*abji
+    return (T1, T2)
+
+
+def level(H,expr):
+
+    pretty_dummies_dict = {
+    'above': 'defg',
+    'below': 'lmno',
+    'general': 'pqrst'
+    }
+
+    #display(Markdown
+    #    (rf"""Calculating 4 nested commutators"""))
+    C = Commutator
+
+    T1, T2 = get_CC_operators()
+    T = T1 + T2
+    comm1 = wicks(C(H, T))
+    comm1 = evaluate_deltas(comm1)
+    comm1 = substitute_dummies(comm1)
+
+    T1, T2 = get_CC_operators()
+    T = T1 + T2
+    comm2 = wicks(C(comm1, T))
+    comm2 = evaluate_deltas(comm2)
+    comm2 = substitute_dummies(comm2)
+
+    T1, T2 = get_CC_operators()
+    T = T1 + T2
+    comm3 = wicks(C(comm2, T))
+    comm3 = evaluate_deltas(comm3)
+    comm3 = substitute_dummies(comm3)
+
+    T1, T2 = get_CC_operators()
+    T = T1 + T2
+    comm4 = wicks(C(comm3, T))
+    comm4 = evaluate_deltas(comm4)
+    comm4 = substitute_dummies(comm4)
+
+    eq = H + comm1 + comm2/2 + comm3/6 + comm4/24
+    eq = eq.expand()
+    eq = evaluate_deltas(eq)
+    eq = substitute_dummies(eq, new_indices=True,
+            pretty_indices=pretty_dummies_dict)
+
+    return eq
+
---
+++ SIGMA.py
@@ -0,0 +1,261 @@
+from sympy import *                                                             
+from sympy.physics.secondquant import F, Fd, wicks, Commutator,evaluate_deltas,AntiSymmetricTensor,NO                  
+from sympy.physics.quantum import Commutator, Dagger, Operator
+from fractions import Fraction
+from sympy.physics.secondquant import simplify_index_permutations
+from sympy.physics.secondquant import PermutationOperator
+from sympy.physics.secondquant import substitute_dummies
+from IPython.display import display, Markdown
+import BCH
+
+def RVECTORS(R0,R1,R2,flavor):                                       
+ display(Markdown
+        (rf""" Computing right sigma amplitudes for {flavor} (skipping summation for dummy variables)"""))
+
+ p, q, r, s = symbols('p,q,r,s', cls=Dummy)
+ f = AntiSymmetricTensor('f', (p,), (q,))
+ pr = NO((Fd(p)*F(q)))
+ v = AntiSymmetricTensor('v', (p, q), (r, s))
+ pqsr = NO(Fd(p)*Fd(q)*F(s)*F(r))
+ ham = f*pr + Rational(1, 4)*v*pqsr
+
+ cc  = BCH.level(ham,"SD")
+ 
+ i,j,k = symbols('i,j,k', below_fermi=True)
+ a,b,c = symbols('a,b,c', above_fermi=True)
+
+ if flavor == "IP": 
+  sig11 = evaluate_deltas(wicks(Fd(i)*(cc*R1-R1*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'abcdefg'}
+  sig11 = substitute_dummies(sig11,new_indices=True, pretty_indices=index_rule)
+ if flavor == "EA": 
+  sig11 = evaluate_deltas(wicks(F(a)*(cc*R1-R1*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'ijklmno','above':  'bcdefg'}
+  sig11 = substitute_dummies(sig11,new_indices=True, pretty_indices=index_rule)
+ if flavor == "DIP": 
+  PermutList = [PermutationOperator(i,j)]
+  sig11 = evaluate_deltas(wicks(Fd(i)*Fd(j)*(cc*R1-R1*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'klmno','above':  'abcdefg'}
+  sig11 = substitute_dummies(sig11,new_indices=True, pretty_indices=index_rule)
+  sig11 = simplify_index_permutations(sig11,PermutList)
+ if flavor == "DEA": 
+  PermutList = [PermutationOperator(a,b)]
+  sig11 = evaluate_deltas(wicks(F(b)*F(a)*(cc*R1-R1*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'ijklmno','above':  'cdefg'}
+  sig11 = substitute_dummies(sig11,new_indices=True, pretty_indices=index_rule)
+  sig11 = simplify_index_permutations(sig11,PermutList)
+
+ sigma_11 = Symbol('((\overline{H}_{SS}-E_{cc})R_{1})')
+ final_eq = Eq(sigma_11, sig11)
+ display(final_eq)
+
+
+ if flavor == "IP": 
+  PermutList = [PermutationOperator(i,j)]
+  sig12 = evaluate_deltas(wicks(Fd(i)*(cc*R2-R2*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'abcdefg'}
+  sig12 = substitute_dummies(sig12,new_indices=True, pretty_indices=index_rule)
+  sig12 = simplify_index_permutations(sig12,PermutList)
+ if flavor == "EA": 
+  PermutList = [PermutationOperator(a,b)]
+  sig12 = evaluate_deltas(wicks(F(a)*(cc*R2-R2*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'ijklmno','above':  'bcdefg'}
+  sig12 = substitute_dummies(sig12,new_indices=True, pretty_indices=index_rule)
+  sig12 = simplify_index_permutations(sig12,PermutList)
+ if flavor == "DIP": 
+  PermutList = [PermutationOperator(i,j),PermutationOperator(j,k),PermutationOperator(i,k)]
+  sig12 = evaluate_deltas(wicks(Fd(i)*Fd(j)*(cc*R2-R2*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'klmno','above':  'abcdefg'}
+  sig12 = substitute_dummies(sig12,new_indices=True, pretty_indices=index_rule)
+  sig12 = simplify_index_permutations(sig12,PermutList)
+ if flavor == "DEA": 
+  PermutList = [PermutationOperator(a,b),PermutationOperator(b,c),PermutationOperator(a,c)]
+  sig12 = evaluate_deltas(wicks(F(b)*F(a)*(cc*R2-R2*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'ijklmno','above':  'cdefg'}
+  sig12 = substitute_dummies(sig12,new_indices=True, pretty_indices=index_rule)
+  sig12 = simplify_index_permutations(sig12,PermutList)
+
+ sigma_12 = Symbol('(\overline{H}_{SD}R_{2})')
+ final_eq = Eq(sigma_12, sig12)
+ display(final_eq)
+
+ if flavor == "IP": 
+  sig21 = evaluate_deltas(wicks(Fd(i)*Fd(j)*F(a)*(cc*R1-R1*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'klmno','above':  'bcdefgh'}
+  sig21 = substitute_dummies(sig21,new_indices=True, pretty_indices=index_rule)
+ if flavor == "EA": 
+  sig21 = evaluate_deltas(wicks(Fd(i)*F(b)*F(a)*(cc*R1-R1*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'cdefgh'}
+  sig21 = substitute_dummies(sig21,new_indices=True, pretty_indices=index_rule)
+ if flavor == "DIP": 
+  PermutList = [PermutationOperator(i,j)]
+  sig21 = evaluate_deltas(wicks(Fd(i)*Fd(j)*Fd(k)*F(a)*(cc*R1-R1*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'lmno','above':  'bcdefgh'}
+  sig21 = substitute_dummies(sig21,new_indices=True, pretty_indices=index_rule)
+  sig21 = simplify_index_permutations(sig21,PermutList)
+ if flavor == "DEA": 
+  PermutList = [PermutationOperator(a,b)]
+  sig21 = evaluate_deltas(wicks(Fd(i)*F(c)*F(b)*F(a)*(cc*R1-R1*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'cdefgh'}
+  sig21 = substitute_dummies(sig21,new_indices=True, pretty_indices=index_rule)
+  sig21 = simplify_index_permutations(sig21,PermutList)
+
+ sigma_21 = Symbol('(\overline{H}_{DS}R_{1})')
+ final_eq = Eq(sigma_21, sig21)
+ display(final_eq)
+
+
+ if flavor == "IP": 
+  PermutList = [PermutationOperator(i,j)]
+  sig22 = evaluate_deltas(wicks(Fd(i)*Fd(j)*F(a)*(cc*R2-R2*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'klmno','above':  'bcdefgh'}
+  sig22 = substitute_dummies(sig22,new_indices=True, pretty_indices=index_rule)
+  sig22 = simplify_index_permutations(sig22,PermutList)
+ if flavor == "EA": 
+  PermutList = [PermutationOperator(a,b)]
+  sig22 = evaluate_deltas(wicks(Fd(i)*F(b)*F(a)*(cc*R2-R2*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'cdefgh'}
+  sig22 = substitute_dummies(sig22,new_indices=True, pretty_indices=index_rule)
+  sig22 = simplify_index_permutations(sig22,PermutList)
+ if flavor == "DIP": 
+  PermutList = [PermutationOperator(i,j),PermutationOperator(j,k),PermutationOperator(i,k)]
+  sig22 = evaluate_deltas(wicks(Fd(i)*Fd(j)*Fd(k)*F(a)*(cc*R2-R2*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'lmno','above':  'bcdefgh'}
+  sig22 = substitute_dummies(sig22,new_indices=True, pretty_indices=index_rule)
+  sig22 = simplify_index_permutations(sig22,PermutList)
+ if flavor == "DEA": 
+  PermutList = [PermutationOperator(a,b),PermutationOperator(b,c),PermutationOperator(a,c)]
+  sig22 = evaluate_deltas(wicks(Fd(i)*F(c)*F(b)*F(a)*(cc*R2-R2*cc) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'defgh'}
+  sig22 = substitute_dummies(sig22,new_indices=True, pretty_indices=index_rule)
+  sig22 = simplify_index_permutations(sig22,PermutList)
+
+ sigma_22 = Symbol('((\overline{H}_{DD}-E_{cc})R_{2})')
+ final_eq = Eq(sigma_22, sig22)
+ display(final_eq)
+
+def LVECTORS(L0,L1,L2,flavor):                                       
+ display(Markdown
+        (rf""" Computing left sigma amplitudes for {flavor} (skipping summation for dummy variables)"""))
+
+ p, q, r, s = symbols('p,q,r,s', cls=Dummy)
+ f = AntiSymmetricTensor('f', (p,), (q,))
+ pr = NO((Fd(p)*F(q)))
+ v = AntiSymmetricTensor('v', (p, q), (r, s))
+ pqsr = NO(Fd(p)*Fd(q)*F(s)*F(r))
+ ham = f*pr + Rational(1, 4)*v*pqsr
+
+ cc  = BCH.level(ham,"SD")
+ 
+ i,j,k = symbols('i,j,k', below_fermi=True)
+ a,b,c = symbols('a,b,c', above_fermi=True)
+
+ if flavor == "IP": 
+  sig11 = evaluate_deltas(wicks((L1*cc-cc*L1)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'abcdefg'}
+  sig11 = substitute_dummies(sig11,new_indices=True, pretty_indices=index_rule)
+ if flavor == "EA": 
+  sig11 = evaluate_deltas(wicks((L1*cc-cc*L1)*Fd(a) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'ijklmno','above':  'bcdefg'}
+  sig11 = substitute_dummies(sig11,new_indices=True, pretty_indices=index_rule)
+ if flavor == "DIP": 
+  PermutList = [PermutationOperator(i,j)]
+  sig11 = evaluate_deltas(wicks((L1*cc-cc*L1)*F(j)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'klmno','above':  'abcdefg'}
+  sig11 = substitute_dummies(sig11,new_indices=True, pretty_indices=index_rule)
+  sig11 = simplify_index_permutations(sig11,PermutList)
+ if flavor == "DEA": 
+  PermutList = [PermutationOperator(a,b)]
+  sig11 = evaluate_deltas(wicks((L1*cc-cc*L1)*Fd(a)*Fd(b) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'ijklmno','above':  'cdefg'}
+  sig11 = substitute_dummies(sig11,new_indices=True, pretty_indices=index_rule)
+  sig11 = simplify_index_permutations(sig11,PermutList)
+
+ sigma_11 = Symbol('(L_{1}(\overline{H}_{SS}-E_{cc}))')
+ final_eq = Eq(sigma_11, sig11)
+ display(final_eq)
+
+
+ if flavor == "IP": 
+  PermutList = [PermutationOperator(i,j)]
+  sig12 = evaluate_deltas(wicks((L2*cc-cc*L2)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'abcdefg'}
+  sig12 = substitute_dummies(sig12,new_indices=True, pretty_indices=index_rule)
+  sig12 = simplify_index_permutations(sig12,PermutList)
+ if flavor == "EA": 
+  PermutList = [PermutationOperator(a,b)]
+  sig12 = evaluate_deltas(wicks((L2*cc-cc*L2)*Fd(a) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'ijklmno','above':  'bcdefg'}
+  sig12 = substitute_dummies(sig12,new_indices=True, pretty_indices=index_rule)
+  sig12 = simplify_index_permutations(sig12,PermutList)
+ if flavor == "DIP": 
+  PermutList = [PermutationOperator(i,j),PermutationOperator(j,k),PermutationOperator(i,k)]
+  sig12 = evaluate_deltas(wicks((L2*cc-cc*L2)*F(j)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'klmno','above':  'abcdefg'}
+  sig12 = substitute_dummies(sig12,new_indices=True, pretty_indices=index_rule)
+  sig12 = simplify_index_permutations(sig12,PermutList)
+ if flavor == "DEA": 
+  PermutList = [PermutationOperator(a,b),PermutationOperator(b,c),PermutationOperator(a,c)]
+  sig12 = evaluate_deltas(wicks((L2*cc-cc*L2)*Fd(a)*Fd(b) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'ijklmno','above':  'cdefg'}
+  sig12 = substitute_dummies(sig12,new_indices=True, pretty_indices=index_rule)
+  sig12 = simplify_index_permutations(sig12,PermutList)
+
+ sigma_12 = Symbol('(L_{2}\overline{H}_{DS})')
+ final_eq = Eq(sigma_12, sig12)
+ display(final_eq)
+
+ if flavor == "IP": 
+  sig21 = evaluate_deltas(wicks((L1*cc-cc*L1)*Fd(a)*F(j)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'klmno','above':  'bcdefgh'}
+  sig21 = substitute_dummies(sig21,new_indices=True, pretty_indices=index_rule)
+ if flavor == "EA": 
+  sig21 = evaluate_deltas(wicks((L1*cc-cc*L1)*Fd(a)*Fd(b)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'cdefgh'}
+  sig21 = substitute_dummies(sig21,new_indices=True, pretty_indices=index_rule)
+ if flavor == "DIP": 
+  PermutList = [PermutationOperator(i,j)]
+  sig21 = evaluate_deltas(wicks((L1*cc-cc*L1)*Fd(a)*F(k)*F(j)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'lmno','above':  'bcdefgh'}
+  sig21 = substitute_dummies(sig21,new_indices=True, pretty_indices=index_rule)
+  sig21 = simplify_index_permutations(sig21,PermutList)
+ if flavor == "DEA": 
+  PermutList = [PermutationOperator(a,b)]
+  sig21 = evaluate_deltas(wicks((L1*cc-cc*L1)*Fd(a)*Fd(b)*Fd(c)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'cdefgh'}
+  sig21 = substitute_dummies(sig21,new_indices=True, pretty_indices=index_rule)
+  sig21 = simplify_index_permutations(sig21,PermutList)
+
+ sigma_21 = Symbol('(L_{1}\overline{H}_{SD})')
+ final_eq = Eq(sigma_21, sig21)
+ display(final_eq)
+
+
+ if flavor == "IP": 
+  PermutList = [PermutationOperator(i,j)]
+  sig22 = evaluate_deltas(wicks((L2*cc-cc*L2)*Fd(a)*F(j)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'klmno','above':  'bcdefgh'}
+  sig22 = substitute_dummies(sig22,new_indices=True, pretty_indices=index_rule)
+  sig22 = simplify_index_permutations(sig22,PermutList)
+ if flavor == "EA": 
+  PermutList = [PermutationOperator(a,b)]
+  sig22 = evaluate_deltas(wicks((L2*cc-cc*L2)*Fd(a)*Fd(b)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'cdefgh'}
+  sig22 = substitute_dummies(sig22,new_indices=True, pretty_indices=index_rule)
+  sig22 = simplify_index_permutations(sig22,PermutList)
+ if flavor == "DIP": 
+  PermutList = [PermutationOperator(i,j),PermutationOperator(j,k),PermutationOperator(i,k)]
+  sig22 = evaluate_deltas(wicks((L2*cc-cc*L2)*Fd(a)*F(k)*F(j)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'lmno','above':  'bcdefgh'}
+  sig22 = substitute_dummies(sig22,new_indices=True, pretty_indices=index_rule)
+  sig22 = simplify_index_permutations(sig22,PermutList)
+ if flavor == "DEA": 
+  PermutList = [PermutationOperator(a,b),PermutationOperator(b,c),PermutationOperator(a,c)]
+  sig22 = evaluate_deltas(wicks((L2*cc-cc*L2)*Fd(a)*Fd(b)*Fd(c)*F(i) , keep_only_fully_contracted=True))
+  index_rule = {'below':  'jklmno','above':  'defgh'}
+  sig22 = substitute_dummies(sig22,new_indices=True, pretty_indices=index_rule)
+  sig22 = simplify_index_permutations(sig22,PermutList)
+
+ sigma_22 = Symbol('(L_{2}(\overline{H}_{DD}-E_{cc}))')
+ final_eq = Eq(sigma_22, sig22)
+ display(final_eq)
